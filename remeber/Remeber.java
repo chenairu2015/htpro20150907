580841e9d5a5cf19a9e484c313d41a4951d7764b
陈爱茹 <kukuxue@126.com>
2015-09-09 09:31:18 +0800

    根目录路径，可以指定绝对路径

---
+++ remeber/Remeber.java
@@ -0,0 +1,7 @@
+public class Remeber {
+	public static void main(String[] args) {
+		// 根目录路径，可以指定绝对路径，比如 /var/www/attached/
+		// String rootPath = pageContext.getServletContext().getRealPath("/") +
+		// "attached/";
+	}
+}
