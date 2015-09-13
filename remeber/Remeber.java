ffce26339b8bab101021e589e3606393038db62b
陈爱茹 <1106989757@qq.com>
2015-09-13 18:45:02 +0800

    Revert "根目录路径，可以指定绝对路径"
    This reverts commit 580841e9d5a5cf19a9e484c313d41a4951d7764b.

--- remeber/Remeber.java
+++
@@ -1,7 +0,0 @@
-public class Remeber {
-	public static void main(String[] args) {
-		// 根目录路径，可以指定绝对路径，比如 /var/www/attached/
-		// String rootPath = pageContext.getServletContext().getRealPath("/") +
-		// "attached/";
-	}
-}
